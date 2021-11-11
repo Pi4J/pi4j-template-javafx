2fc8f340b643ef9e953ed906d0d96c9c3a9c221a
dieter <dieter.holz@fhnw.ch>
2021-11-11 11:58:24 +0100

    fhnw logo added

--- src/test/java/com/pi4j/jfx/mvcapp/model/ExampleModelTest.java
+++ src/test/java/com/pi4j/jfx/mvcapp/model/ExampleModelTest.java
@@ -5,5 +5,5 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class ExampleModelTest {
 
-    // ExampleModel consists of a list of ObservableValues. No additional logic. Therefore no need to test anything here.
+    // ExampleModel consists of a list of ObservableValues. No additional logic. Therefore, no need to test anything here.
 }
