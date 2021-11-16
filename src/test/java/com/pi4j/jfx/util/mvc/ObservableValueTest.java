d92d1da3af9494d4383f891613227b52eb1223b8
dieter <dieter.holz@fhnw.ch>
2021-11-16 11:26:57 +0100

    added TestCases to ObservableValueTest

--- src/test/java/com/pi4j/jfx/util/mvc/ObservableValueTest.java
+++ src/test/java/com/pi4j/jfx/util/mvc/ObservableValueTest.java
@@ -1,6 +1,7 @@
 package com.pi4j.jfx.util.mvc;
 
 import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
 
 import org.junit.jupiter.api.Test;
 
@@ -27,27 +28,59 @@ class ObservableValueTest {
     }
 
     @Test
-    void testSetValue(){
+    void testSetValue() {
         //given
-        ObservableValue<Boolean> v = new ObservableValue<>(false);
-
-        AtomicInteger counter = new AtomicInteger(0);
+        ObservableValue<Boolean> observableValue = new ObservableValue<>(false);
 
         //when
-        v.onChange((oldValue, newValue) -> counter.getAndIncrement());
+        observableValue.setValue(true);
 
         //then
-        assertEquals(1, counter.get());   // listener is call on registration
+        assertTrue(observableValue.getValue());
 
         //when
-        v.setValue(false);
+        observableValue.setValue(false);
+
+        //then
+        assertFalse(observableValue.getValue());
+    }
+
+    @Test
+    void testSetListener(){
+        //given
+        String                  initialValue    = "initial Value";
+        String                  firstValue      = "first value";
+        ObservableValue<String> observableValue = new ObservableValue<>(initialValue);
+
+        AtomicInteger           counter  = new AtomicInteger(0);
+        AtomicReference<String> foundOld = new AtomicReference<>();
+        AtomicReference<String> foundNew = new AtomicReference<>();
+
+        //when
+        observableValue.onChange((oldValue, newValue) -> {
+            counter.getAndIncrement();
+            foundOld.set(oldValue);
+            foundNew.set(newValue);
+        });
+
+        //then
+        assertEquals(1, counter.get());                       // listener is called on registration
+        assertEquals(initialValue, foundOld.get());           // initial value of oldValue is current value
+        assertEquals(initialValue, foundNew.get());           // current value
+
+        //when
+        observableValue.setValue(initialValue);
 
         //then
         assertEquals(1, counter.get()); // value stays the same; listener is not called
 
         //when
-        v.setValue(true);
+        observableValue.setValue(firstValue);
+
+        //then
         assertEquals(2, counter.get()); // value has changed; listener is called
+        assertEquals(initialValue, foundOld.get());
+        assertEquals(firstValue, foundNew.get());
     }
 
 }
