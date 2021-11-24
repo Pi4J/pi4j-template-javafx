d70567c173b40ee066a704ef84f1b8e28b95c74b
dieter <dieter.holz@fhnw.ch>
2021-11-24 11:26:19 +0100

    ViewMixin onChange(Property) added (very convenient)

--- src/main/java/com/pi4j/mvc/util/mvcbase/ViewMixin.java
+++ src/main/java/com/pi4j/mvc/util/mvcbase/ViewMixin.java
@@ -1,9 +1,12 @@
 package com.pi4j.mvc.util.mvcbase;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Function;
 
 import javafx.application.Platform;
+import javafx.beans.property.DoubleProperty;
+import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.Property;
 import javafx.scene.text.Font;
 
@@ -61,23 +64,16 @@ public interface ViewMixin<M,  C extends ControllerBase<M>> extends Projector<M,
 
     /**
      *
-     *
      */
-    class Converter<V>{
-        private final ObservableValue<V> observableValue;
-
-        Converter(ObservableValue<V> observableValue) {
-            this.observableValue = observableValue;
-        }
+    record Converter<V>(ObservableValue<V> observableValue) {
 
         /**
          * Second (optional) step for registering an observer to specify a converter-function
          *
          * @param converter the function converting the type of 'ObservableValue' into the type of the 'Property'
-         *
          * @return an Updater to specify the 'GUI-Property' that needs to be updated if 'ObservableValue' changes
          */
-        public <R> Updater<V, R> convertedBy(Function<V, R> converter){
+        public <R> Updater<V, R> convertedBy(Function<V, R> converter) {
             return new Updater<>(observableValue, converter);
         }
 
@@ -95,30 +91,42 @@ public interface ViewMixin<M,  C extends ControllerBase<M>> extends Projector<M,
          *
          * @param listener whatever needs to be done on GUI when observableValue changes
          */
-        public void execute(ObservableValue.ValueChangeListener<V> listener){
+        public void execute(ObservableValue.ValueChangeListener<V> listener) {
             observableValue.onChange((oldValue, newValue) -> Platform.runLater(() -> listener.update(oldValue, newValue)));
         }
     }
 
-    class Updater<V, P> {
-        private final ObservableValue<V> observableValue;
-        private final Function<V, P>     converter;
-
-        Updater(ObservableValue<V> observableValue, Function<V, P> converter){
-            this.observableValue = observableValue;
-            this.converter       = converter;
-        }
+    record Updater<V, P>(ObservableValue<V> observableValue, Function<V, P> converter) {
 
         /**
          * Registers an observer that will keep observableValue and GUI-Property in sync by applying the specified converter.
          *
          * @param property GUI-Property that will be updated when observableValue changes
          */
-        public void update(Property<? super P> property){
+        public void update(Property<? super P> property) {
             observableValue.onChange((oldValue, newValue) -> {
                 P convertedValue = converter.apply(newValue);
                 Platform.runLater(() -> property.setValue(convertedValue));
             });
-         }
+        }
+    }
+
+    default <V> ActionTrigger<V> onChangeOf(Property<V> property){
+        return new ActionTrigger<>(property);
+    }
+
+    default ActionTrigger<Double> onChangeOf(DoubleProperty property){
+        return new ActionTrigger<>(property);
+    }
+
+    default ActionTrigger<Integer> onChangeOf(IntegerProperty property){
+        return new ActionTrigger<>(property);
+    }
+
+    record ActionTrigger<V>(Property<? super V> property) {
+
+        public void triggerAction(Consumer<V> action) {
+            property.addListener((observableValue, oldValue, newValue) -> action.accept((V) newValue));
+        }
     }
 }
