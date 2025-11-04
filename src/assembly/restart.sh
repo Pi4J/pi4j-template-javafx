89c8948d6eb8fec67812fca8536b62cd5589bf49
DieterHolz <dieter.holz@fhnw.ch>
2025-11-04 22:20:40 +0100

    upgrade to JDK, JFX 25

--- src/assembly/restart.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd "$1"
-pkill java
-DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java --module-path "$HOME/openjfx/extracted_files/openjfx/lib" --add-modules javafx.controls -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx1G -jar "$2".jar
-exit 0
--- src/assembly/start.sh
+++ src/assembly/start.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 cd "$1"
-./download_openjfx.sh
 pkill java
-DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java --module-path "$HOME/openjfx/extracted_files/openjfx/lib" --add-modules javafx.controls -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx1G -jar "$2".jar
+DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java --add-modules javafx.controls --enable-native-access=javafx.controls,javafx.graphics,ALL-UNNAMED -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx1G -jar "$2".jar
 exit 0
