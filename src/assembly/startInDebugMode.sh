93bd0fb197aaa7c1d895eb53892176cbc7ee55ae
DieterHolz <dieter.holz@fhnw.ch>
2025-11-06 12:57:23 +0100

    use jdk from sdkman

--- src/assembly/startInDebugMode.sh
+++ src/assembly/startInDebugMode.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd "$1"
 pkill java
-DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority java --add-modules javafx.controls --enable-native-access=javafx.controls,javafx.graphics,ALL-UNNAMED -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx1G -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -jar "$2".jar
+DISPLAY=:0 XAUTHORITY=/home/pi/.Xauthority /home/pi/.sdkman/candidates/java/current/bin/java --add-modules javafx.controls --enable-native-access=javafx.controls,javafx.graphics,ALL-UNNAMED -Dsun.java2d.opengl=True -XX:+UseZGC -Xmx1G -agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005 -jar "$2".jar
 exit 0
